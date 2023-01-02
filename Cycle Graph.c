74b59e3ec2c7876db45eaab8661b3099cb5cb78b
koi151 <111631682+koi151@users.noreply.github.com>
2023-01-02 20:52:26 +0700

    Create Cycle Graph.c

---
+++ Cycle Graph.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#define Max_Vertices 100
+#define Max_Elements 100
+#define WHITE 0 
+#define BLACK 1
+#define GRAY 2
+int has_cycle = 0;
+
+typedef struct {
+    int A[Max_Vertices][Max_Vertices];
+    int n, m;
+}Graph;
+
+typedef struct {
+    int data[Max_Elements];
+    int size;
+}List;
+
+//---------------------------List-------------------------------
+
+void make_null_List(List *L)
+{
+    L->size = 0;
+}
+
+int empty_List(List L)
+{
+    return L.size == 0;
+}
+
+void push_back(List *L, int x)
+{
+    L->data[L->size] = x;
+    L->size ++;
+}
+
+int element_at(List L, int pos)
+{
+    return L.data[pos-1];
+}
+
+//---------------------------Graph-------------------------------
+
+void init_Graph(Graph *G, int n)
+{
+    G->n = n;
+    G->m = 0;
+    int i, j;
+    for (i = 1; i <= G->n; i++)
+        for (j = 1; j <= G->n; j++)
+            G->A[i][j] = 0;
+}
+
+void add_Edge(Graph *G, int x, int y)
+{
+    G->A[x][y] = 1;
+    G->m ++;
+}
+
+int adjacent(Graph G, int x, int y)
+{
+    return G.A[x][y] == 1;
+}
+
+List neighbors(Graph G, int x)
+{
+    List L;
+    make_null_List(&L);
+    int i;
+    for (i = 1; i <= G.n; i++)
+        if (G.A[x][i]) push_back(&L, i);
+    return L;
+}
+
+void DFS(Graph G, int u, int color[])
+{
+    int i;
+    color[u] = GRAY;
+
+    List Nei = neighbors(G,u);
+    for (i = 0; i < Nei.size; i++)
+    {
+        int v = Nei.data[i];
+        if (color[v] == WHITE)
+            DFS(G,v, color);
+        else if (color[v] == GRAY)
+        {
+            has_cycle = 1;
+            return;
+        } else continue;
+    }
+    color[u] = BLACK;
+}
+
+//=============================================
+int main ()
+{
+    freopen("test2.txt", "r", stdin);
+    Graph G;
+    int n, m, u, v, i;
+    scanf("%d%d", &n, &m);
+    init_Graph(&G, n);
+
+    for (i = 1; i <= m; i++)
+    {
+        scanf("%d%d", &u, &v);
+        add_Edge(&G, u, v);
+    }    
+
+    int color[Max_Vertices];
+    for (i = 1; i <= G.n; i++)
+        color[i] = 0;
+
+    DFS(G, 3, color);
+    if (has_cycle) printf("Graph contain cycle");
+    else printf("Graph does not contain cycle");
+
+    return 0;
+}
