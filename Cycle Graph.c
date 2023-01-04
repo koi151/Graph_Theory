a7e8062be08b632431e6d94a706fa074abf6b292
koi151 <111631682+koi151@users.noreply.github.com>
2023-01-04 20:23:25 +0700

    Update Cycle Graph.c

--- Cycle Graph.c
+++ Cycle Graph.c
@@ -1,59 +1,29 @@
 #include <stdio.h>
-#define Max_Vertices 100
-#define Max_Elements 100
-#define WHITE 0 
-#define BLACK 1
-#define GRAY 2
 
+#define Max_Vertices 100
+#define WHITE 0
+#define GRAY 1
+#define BLACK 2
 
 typedef struct {
     int A[Max_Vertices][Max_Vertices];
     int n, m;
 }Graph;
 
-typedef struct {
-    int data[Max_Elements];
-    int size;
-}List;
-
-//---------------------------List-------------------------------
-
-void make_null_List(List *L)
-{
-    L->size = 0;
-}
-
-int empty_List(List L)
-{
-    return L.size == 0;
-}
-
-void push_back(List *L, int x)
-{
-    L->data[L->size] = x;
-    L->size ++;
-}
-
-int element_at(List L, int pos)
-{
-    return L.data[pos-1];
-}
-
-//---------------------------Graph-------------------------------
-
 void init_Graph(Graph *G, int n)
 {
     G->n = n;
     G->m = 0;
     int i, j;
-    for (i = 1; i <= G->n; i++)
-        for (j = 1; j <= G->n; j++)
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
             G->A[i][j] = 0;
 }
 
 void add_Edge(Graph *G, int x, int y)
 {
     G->A[x][y] = 1;
+    G->A[y][x] = 1;
     G->m ++;
 }
 
@@ -62,59 +32,124 @@ int adjacent(Graph G, int x, int y)
     return G.A[x][y] == 1;
 }
 
-List neighbors(Graph G, int x)
+int color[Max_Vertices], parent[Max_Vertices];
+int has_cycle = 0, begin, end;
+
+void DFS(Graph G, int x, int p)
 {
-    List L;
-    make_null_List(&L);
     int i;
+    color[x] = GRAY;
+    parent[x] = p;
+
     for (i = 1; i <= G.n; i++)
-        if (G.A[x][i]) push_back(&L, i);
-    return L;
-}
-
-int color[Max_Vertices];
-int has_cycle = 0;
-
-void DFS(Graph G, int u)
-{
-    int i;
-    color[u] = GRAY;
-
-    List Nei = neighbors(G,u);
-    for (i = 0; i < Nei.size; i++)
     {
-        int v = Nei.data[i];
-        if (color[v] == WHITE)
-            DFS(G,v);
-        else if (color[v] == GRAY)
+        if (adjacent(G, x, i))
         {
-            has_cycle = 1;
-            return;
+            if (i == p) continue;
+            if (color[i] == WHITE)
+                DFS(G, i, x);
+            else if (color[i] == GRAY)
+            {
+                has_cycle ++;
+                begin = x;
+                end = i;
+                
+            }
         }
     }
-    color[u] = BLACK;
+    color[x] = BLACK;    
 }
 
-//=============================================
-int main ()
+//======================================
+int main()
 {
-    freopen("test2.txt", "r", stdin);
-    Graph G;
-    int n, m, u, v, i;
-    scanf("%d%d", &n, &m);
-    init_Graph(&G, n);
+    freopen("test.txt", "r", stdin);
+	int n, m, u, v, i, j;
+	scanf("%d %d", &n, &m);
+	Graph G;
+	init_Graph(&G, n);
 
-    for (i = 1; i <= m; i++)
+	for (int i = 1; i <= m; i++)
     {
-        scanf("%d%d", &u, &v);
-        add_Edge(&G, u, v);
-    }    
+		scanf("%d %d", &u, &v);
+		add_Edge(&G, u, v);
+	}
 
-    for (i = 1; i <= G.n; i++)
-        color[i] = 0;
+	for (int i = 1; i <= n; i++)
+    {
+		color[i] = WHITE;
+		parent[i] = -1;
+	}
+	
+	for (int i = 1; i <= n; i++) 
+		if (color[i] == WHITE) 
+			DFS(G, i, -1);
+	
+	if (has_cycle != 0)
+    {
+        printf("Graph has cycle (and maybe more): ");
+		int A[Max_Vertices];
+		A[0] = begin;
+		int b = begin, e = end;
+        int i = 0;
+		do 
+        {
+			b = parent[b];
+			A[++i] = b;
+		} while (b != e);
 
-    DFS(G, 3);
-    if (has_cycle) printf("Graph cycled");
-    else printf("Graph does not contain cycle");
-    return 0;
+        for (int j = i; j >= 0; j--)
+			printf("%d ", A[j]);
+		printf("%d\n", A[i]);
+
+	}
+	else printf("Graph does not have cycle");
+	return 0;
 }
+
+// int main()
+// {
+//     freopen("test.txt", "r", stdin);
+// 	int n, m, u, v, i, j;
+// 	scanf("%d %d", &n, &m);
+
+// 	Graph G;
+// 	init_Graph(&G, n);
+
+// 	for (int i = 1; i <= m; i++)
+//     {
+// 		scanf("%d %d", &u, &v);
+// 		add_Edge(&G, u, v);
+// 	}
+
+// 	for (int i = 1; i <= n; i++)
+//     {
+// 		color[u] = WHITE;
+// 		parent[u] = -1;
+// 	}
+
+// 	for (i = 1; i <= n; i++)
+// 		if (color[i] == WHITE) 
+// 			DFS(G, i, -1);
+	
+// 	if (has_cycle != 0) 
+//     {
+// 		int A[Max_Vertices], i = 0;
+// 		A[0] = begin;
+// 		int u = begin, v = end;
+// 		do
+//         {
+// 			u = parent[u];
+			
+// 			A[++i] = u;
+// 		} while (u != v);
+
+// 		for (int j = i; j >= 0; j--)
+// 			printf("%d ", A[j]);
+
+// 		printf("%d\n", A[i]);
+// 	}
+// 	else printf("-1");
+
+// 	return 0;
+// }
